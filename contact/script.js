6857a786a546fcbc4dcd54c93156abc1fede577a
sebaro <esebaro@gmail.com>
2024-09-22 21:55:09 +0300

    Contact archive old messages

--- contact/script.js
+++ contact/script.js
@@ -12,6 +12,7 @@ function create(type, properties, event, listener) {
 
 function request(post) {
 	var url = 'https://sebaro-apps.vercel.app/api/contact';
+	//var url = './messages.php';
 	var xhr = new XMLHttpRequest();
 	var method = 'GET';
 	if (post) {
@@ -52,35 +53,53 @@ function request(post) {
 }
 
 function show() {
-	var mbox, mid, msubject, mauthor, mcontent, mdate, mstyle;
+	var mbox, mid, msubject, mauthor, mcontent, mdate, mstyle, osubject, obox;
 	var rbox, rbutton;
-	var subjects = [];
+	var subjects = {};
+	var active = [];
+	var archive = [];
+	var threshold = 100, days, sd, cd = new Date().getTime();
 	var ns, nl;
 	for (var i = 0; i < data.length; i++) {
-		if (subjects.indexOf(data[i]['subject']) == -1) {
-			subjects.push(data[i]['subject']);
+		subjects[data[i]['subject']] = data[i]['date'];
+	}
+	for (var subject in subjects) {
+		days = 0;
+		try {
+			sd = new Date(subjects[subject].replace('/', 'T')).getTime();
+			days = (cd / 1000 - sd / 1000 ) / (60 * 60 * 24);
+		}
+		catch(e) {}
+		if (days <= threshold) {
+			active.push(subject);
+		}
+		else {
+			archive.push(subject);
 		}
 	}
-	for (var i = 0; i < subjects.length; i++) {
+	active = active.reverse();
+	for (var i = 0; i < active.length; i++) {
 		ns = true;
 		for (var j = 0; j < data.length; j++) {
-			if (data[j]['subject'] == subjects[i]) {
+			if (data[j]['subject'] == active[i]) {
 				mstyle = (ns) ? "entry" : "rentry";
 				mid = data[j]['author'] + '@' + data[j]['date'];
 				mbox = create('div', {className: mstyle, id: mid});
-				msubject = create('div', {className: 'subject', textContent: data[j]['subject']});
 				mstyle = (data[j]['author'] == 'sebaro') ? "authori" : "author";
 				mauthor = create('div', {className: mstyle, textContent: data[j]['author']});
 				mcontent = data[j]['message'];
 				if (mcontent) mcontent = mcontent.replace(/(https?:\/\/([-\w\.]+[-\w])+(:\d+)?(\/([\w/_\.#-\@~]*(\?\S+)?[^\.,\s\)])?)?)/g, '<a href="$1">$1</a>');
 				mcontent = create('div', {className: 'message', innerHTML: mcontent});
 				mdate = create('div', {className: 'date', textContent: data[j]['date']});
-				nl = create('br');
-				mbox.appendChild(msubject);
+				if (ns) {
+					msubject = create('div', {className: 'subject', textContent: data[j]['subject']});
+					mbox.appendChild(msubject);
+				}
 				mbox.appendChild(mauthor);
 				mbox.appendChild(mdate);
 				mbox.appendChild(mcontent);
 				messages.appendChild(mbox);
+				nl = create('br');
 				messages.appendChild(nl);
 				ns = false;
 			}
@@ -88,10 +107,50 @@ function show() {
 		rbox = create('div', {className: 'rbox'});
 		rbutton = create('div', {className: 'rbutton', textContent: 'Reply'}, 'click', function() {
 			window.scrollTo(0, 0);
-			subject.value = subjects[this];
+			subject.value = active[this];
 		}.bind(i));
 		rbox.appendChild(rbutton);
 		messages.appendChild(rbox);
+		nl = create('br');
+		messages.appendChild(nl);
+	}
+	if (archive.length > 0) {
+		nl = create('br');
+		messages.appendChild(nl);
+		nl = create('br');
+		messages.appendChild(nl);
+	}
+	archive = archive.reverse();
+	for (var i = 0; i < archive.length; i++) {
+		osubject = create('div', {className: 'osubject', textContent: archive[i]}, 'click', function() {
+			var mstyle = document.getElementById('oldmessage' + this).style.display;
+			if (!mstyle || mstyle == 'none') document.getElementById('oldmessage' + this).style.display = 'block';
+			else if (mstyle == 'block') document.getElementById('oldmessage' + this).style.display = 'none';
+		}.bind(i));
+		messages.appendChild(osubject);
+		obox = create('div', {className: 'omessage', id: 'oldmessage' + i});
+		messages.appendChild(obox);
+		ns = true;
+		for (var j = 0; j < data.length; j++) {
+			if (data[j]['subject'] == archive[i]) {
+				mstyle = (ns) ? "oentry" : "rentry";
+				mid = data[j]['author'] + '@' + data[j]['date'];
+				mbox = create('div', {className: mstyle, id: mid});
+				mstyle = (data[j]['author'] == 'sebaro') ? "authori" : "author";
+				mauthor = create('div', {className: mstyle, textContent: data[j]['author']});
+				mcontent = data[j]['message'];
+				if (mcontent) mcontent = mcontent.replace(/(https?:\/\/([-\w\.]+[-\w])+(:\d+)?(\/([\w/_\.#-\@~]*(\?\S+)?[^\.,\s\)])?)?)/g, '<a href="$1">$1</a>');
+				mcontent = create('div', {className: 'message', innerHTML: mcontent});
+				mdate = create('div', {className: 'date', textContent: data[j]['date']});
+				mbox.appendChild(mauthor);
+				mbox.appendChild(mdate);
+				mbox.appendChild(mcontent);
+				obox.appendChild(mbox);
+				ns = false;
+			}
+		}
+		nl = create('br');
+		messages.appendChild(nl);
 		nl = create('br');
 		messages.appendChild(nl);
 	}
@@ -149,8 +208,8 @@ submit.addEventListener('click', function() {
     warn = "Message field is missing!";
   }
   if (!warn) {
-		post['author'] = author.value.replace('&amp;', '&').replace('&', '&amp;');
-		post['subject'] = subject.value.replace('&amp;', '&').replace('&', '&amp;');
+		post['author'] = author.value.replace('&amp;', '&').replace('&', '&amp;').trim();
+		post['subject'] = subject.value.replace('&amp;', '&').replace('&', '&amp;').trim();
 		post['message'] = message.value.replace('&amp;', '&').replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace(/\r/g, '');
 		post['date'] = new Date().toISOString().replace(/:[^:]+$/, '').replace('T', '/');
 		//console.log(post);
